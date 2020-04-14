8b2a194dcdefba2f63b6e54f0b6f58f634330de2
Dalia <dalia.sawaya@flatironschool.com>
2020-04-14 17:59:39 -0400

    earlier commits did not go through

--- js_project_test_frontend/src/problem.js
+++ js_project_test_frontend/src/problem.js
@@ -1,6 +1,72 @@
 class Problem {
     constructor(problem){
+        this.id = problem.id
         this.name = problem.name
         this.description = problem.description
     }
+
+    static newProblem(id) {
+        let body = document.getElementById('container')
+        let form = 
+        `
+            <form id="new-user-and-new-problem-form">
+                <label>What's your problem?:</label>
+                <input type="text" id="problem-name"/>
+                <label>Describe it:</label>
+                <input type="text" id="problem-description"/>
+                <input type="submit"/>
+            </form>
+        `
+        body.insertAdjacentHTML( 'beforeend', form )
+        let newForm = document.getElementById('new-user-and-new-problem-form')
+        newForm.addEventListener('submit', function(e){
+            e.preventDefault()
+            fetch('http://localhost:3000/api/v1/problems', {
+                method: "POST",
+                headers:{
+                    "Content-Type": "application/json",
+                    "Accept": "application/json"
+                },
+                body: JSON.stringify(
+                    {
+                        problem: {
+                            name: e.target.children[1].value,
+                            description: e.target.children[3].value,
+                            user_id: id
+                        }
+                    }
+                )
+            })
+            .then(resp => resp.json())
+            .then(function(json) {
+                let newProblem = new Problem(json)
+                newProblem.appendProblem()
+                newForm.reset()
+            })
+        })
+    }
+
+    appendProblem(){
+        let problems = document.getElementsByClassName('problems-container')
+        //problems is an html collection
+        let li = document.createElement('li')
+        li.setAttribute('data-id', this.id)
+        li.setAttribute('style', "list-style-type:none")
+        //this is just so I don't see actually bullets
+        li.innerHTML = `${this.name} ~ ${this.description}`
+        let solveForm = `<button type="button" id="${this.id}" class="solve-problem"> Solve </button>`
+        li.insertAdjacentHTML('beforeend', solveForm)
+        let button = document.getElementsByClassName('solve-problem')
+        problems[0].append(li)
+        Array.from(button).forEach(function (b) {
+            b.addEventListener('click', function(e){
+                e.preventDefault()
+                fetch(`http://localhost:3000/api/v1/problems/${e.target.parentNode.dataset.id}`, {
+                        method: "DELETE"
+                        })
+                        e.target.parentElement.remove();
+            })
+        })
+    }
+
 }
--- js_project_test_frontend/src/user.js
+++ js_project_test_frontend/src/user.js
@@ -3,4 +3,50 @@ class User {
         this.name = user.name
         this.problems = user.problems
     }
+
+    static createUser(){
+        let newUserForm = document.getElementById('new-user-and-new-problem-form')
+        let newUser = document.getElementById('new-user-body')
+        newUserForm.addEventListener('submit', function(e){
+            e.preventDefault()
+                fetch('http://localhost:3000/api/v1/users', {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Accept": "application/json"
+                    },
+                    body: JSON.stringify(
+                        {
+                            user: {
+                                name: e.target.children[1].value
+                            }
+                        })
+                    })
+                        .then(resp =>  {
+                            return resp.json()
+                        })
+                        .then(user => {
+                            let newUser = new User(user)
+                            newUser.displayUser(user)
+                        })
+        })
+    }
+
+    displayUser(user) {
+        let body = document.getElementById('container')
+        body.innerHTML = ''
+        let userContainer = document.createElement('div')
+        userContainer.setAttribute('data-id', user.id)
+        let id = userContainer.dataset.id
+        userContainer.innerHTML = `Welcome, ${user.name}!`
+        body.append(userContainer)
+        Problem.newProblem(user.id)
+        if (this.problems) {
+            this.problems.forEach(function(problem){
+                let newProblem = new Problem(problem)
+                newProblem.appendProblem()
+            })
+        }
+    }
+
 }
